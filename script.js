e7c7c84959f6529615a3cff44cb823e85c21a130
ChairoConnect <connectchairo@gmail.com>
2024-08-20 07:07:06 +1000

    Update script.js

--- script.js
+++ script.js
@@ -54,8 +54,7 @@ function displayResults(results) {
             panel.innerHTML = `
                 <div class="image-zoom-container">
                     <img src="${result.image_url}" alt="${result.name}">
-                    <br>
-                    <div class="price-panel">${result.price}</div>
+                    <div class="price-panel" style="margin-top:35px;">${result.price}</div>
                 </div>
                 <h3>${result.name}</h3>
                 <p>${result.description}</p>
